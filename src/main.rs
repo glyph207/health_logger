1da27fc3eb448b56d1ccb538b7b58c7693895f43
glyph <nosolonin@outlook.com>
2019-08-06 22:00:43 +0300

    added basic user input processing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::io;
+
 fn main() {
     println!("welcome to health logger!");
+    println!("type 'list' for list of commands, 'exit' or, well, command");
+    let mut user_input = String::new();
+
+    loop {
+        user_input.clear();
+        //println!("user input is {}", user_input);
+        io::stdin().read_line(&mut user_input)
+            .expect("failed to read line");
+
+        match user_input.trim().as_ref() {
+            "list" => {
+                println!("* list");
+                println!("* exit");
+                println!("other commands are in development, sorry");
+            },
+            "exit" => {
+                println!("thanx for using health logger, bye!");
+                break;
+            },
+            _ => println!("unrecognized command, plz try again"),
+        };
+    }
 }
